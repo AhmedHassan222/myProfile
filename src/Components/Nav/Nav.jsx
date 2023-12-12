b15dfca404ffa9e14c6179c116e8a6af3f0f1006
Ahmed Hassan <ah2300487@gmail.com>
2023-12-12 14:18:06 +0200

    reset setting

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,52 +1,18 @@
 
 import { Link } from "react-router-dom"
 import style from "./Nav.module.css"
-import { useEffect, useState } from "react";
 
 export default function Nav() {
-
-    const [isOpen, setIsOpen] = useState(false)
-    function togleMenu() {
-        if (isOpen == false) {
-            setIsOpen(true);
-            console.log('open')
-            document.querySelector('#open').classList.replace('d-block','d-none')
-            document.querySelector('#close').classList.replace('d-none','d-block')
-            document.querySelector(`.${style.aside}`).classList.add(`${style.open}`)
-            document.querySelector(`.${style.aside}`).classList.remove(`${style.close}`)
-
-
-        } else {
-            setIsOpen(false);
-            document.querySelector('#close').classList.replace('d-block','d-none')
-            document.querySelector('#open').classList.replace('d-none','d-block')
-            console.log('close')
-            document.querySelector(`.${style.aside}`).classList.add(`${style.close}`)
-            document.querySelector(`.${style.aside}`).classList.remove(`${style.open}`)
-
-
-
-
-
-        }
-
-    }
 
 
     return <>
         <nav className="navbar navbar-expand-lg bg-white  py-1">
-            <div className="container ">
+            <div className="container">
                 <span className="navbar-brand fs-1 h1">A.</span>
-                <div onClick={togleMenu} >
-                    <div id="open" className="d-block">
-                        <i className={`fa-solid fa-bars fs-4 ${style.smallButton}`}></i>
-                    </div>
-                    <div id="close" className="d-none">
-                        <i className={`fa-solid fa-x  fs-4 ${style.smallButton}`}></i>
-                    </div>
-                </div>
-
-                <div className={`  ${style.aside}`} >
+                <button className={`border-0 bg-transparent fs-5 ${style.smallButton}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon "></span>
+                </button>
+                <div className={`collapse navbar-collapse `} id="navbarSupportedContent">
                     <ul className={`navbar-nav ms-auto mb-2 mb-lg-0 fw-bold ${style.smallview}`}>
                         <li className="nav-item">
                             <Link className={`nav-link mx-3 ${style.linkStyle}`} to='' >HOME</Link>
